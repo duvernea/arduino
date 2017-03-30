804f82f5d0f2d05d9956cfb580ea10127be772de
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-03-30 13:29:04 -0500

    feat: temp sensor - Use 3 digit precision, printToSerial, internal aref

--- temp_sensor/temp_sensor.ino
+++ temp_sensor/temp_sensor.ino
@@ -6,11 +6,18 @@
 */
 
 const int temperaturePin = 0;
-
+const double voltageMax = 1.1;
+const int numBits = 10; // 10 bit adc
 
 void setup()
 {
   Serial.begin(9600);
+  // analogReference(EXTERNAL);
+  // INTERNAL ref = 1.1V
+  // EXTERNAL = apply to AREF
+  // DEFAULT = 5V
+  analogReference(INTERNAL);
+  
 }
 
 void loop()
@@ -22,28 +29,21 @@ void loop()
   voltage = getVoltage(temperaturePin);
   
   // Formula from temperature sensor datasheet:
-  degreesC = (voltage - 0.5) * 100.0;
+  degreesC = (voltage - 0.5) * 100.00;
   degreesF = degreesC * (9.0/5.0) + 32.0;
   
-  Serial.print("voltage: ");
-  Serial.print(voltage);
-  Serial.print("  deg C: ");
-  Serial.print(degreesC);
-  Serial.print("  deg F: ");
-  Serial.println(degreesF);
+  printToSerial(voltage, degreesC, degreesF);
   
   delay(1000); // repeat once per second (change as you wish!)
 }
 
 float getVoltage(int pin)
 {
-  double voltageMax = 5;
-  int numBits = 10;
   return (analogRead(pin) * 1/(pow(2,numBits))*voltageMax);
 }
 void printToSerial(float voltage, float degreesC, float degreesF) {
   Serial.print("voltage: ");
-  Serial.print(voltage);
+  Serial.print(voltage, 3);
   Serial.print("  deg C: ");
   Serial.print(degreesC);
   Serial.print("  deg F: ");
