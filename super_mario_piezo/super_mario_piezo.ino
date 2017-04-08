17c5f7e5a6efa679a40b1dc1c6e3fc3463a36a11
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-04-07 20:19:55 -0500

    feat: Add detail to note duration calc

--- super_mario_piezo/super_mario_piezo.ino
+++ super_mario_piezo/super_mario_piezo.ino
@@ -2,6 +2,8 @@
 #include "mario.h"
 
 #define melodyPin 3
+#define SECONDS_PER_MINUTE 60
+
 int BPM = 150;
 
 Note test_melody[] = {
@@ -23,27 +25,30 @@ void sing(Note song[]) {
   // iterate over the notes of the melody:
     int size = sizeof(song) / sizeof(song[0]);
     for (int thisNote = 0; thisNote < size; thisNote++) {
-      int noteDuration = (int)((1000 * (60 * 4 / BPM)) / song[thisNote].duration + 0.);
+      // note duration in milliseconds
+      int beatsPerMeasure = 4;
+      // currently assumes quarter note gets the beat
+      int beatsPerSecond = (int)(BPM / SECONDS_PER_MINUTE);
+      int secondsPerMeasure = (int) beatsPerMeasure/beatsPerSecond;
+      int noteDuration = (int)secondsPerMeasure / (song[thisNote].duration + 0.);
       buzz(melodyPin, song[thisNote].pitch, noteDuration);
 
       // to separate the notes, set a minimum time between them. + 30% seems to work well.
       delay(noteDuration * 1.30);
       // stop the tone playing:
       buzz(melodyPin, 0, noteDuration);
-     }
+    }
 }
 
 void buzz(int targetPin, long frequency, long length) {
   long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
-  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
-  //// there are two phases to each cycle
+  //// 1E6 microseconds, divided by freq, div by 2 for 50% duty cycle
   long numCycles = frequency * length / 1000; // calculate the number of cycles for proper timing
-  //// multiply frequency, which is really cycles per second, by the number of seconds to
-  //// get the total number of cycles to produce
-  for (long i = 0; i < numCycles; i++) { // for the calculated length of time...
-    digitalWrite(targetPin, HIGH); // write the buzzer pin high to push out the diaphram
-    delayMicroseconds(delayValue); // wait for the calculated delay value
-    digitalWrite(targetPin, LOW); // write the buzzer pin low to pull back the diaphram
-    delayMicroseconds(delayValue); // wait again or the calculated delay value
+  //// total cycles = frequency (cycle/sec) * number of seconds 
+  for (long i = 0; i < numCycles; i++) { 
+    digitalWrite(targetPin, HIGH);
+    delayMicroseconds(delayValue); 
+    digitalWrite(targetPin, LOW); 
+    delayMicroseconds(delayValue); 
   }
 }
