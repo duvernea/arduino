85b50454d64fa36a1f4b93af8cf335a64f9fddbd
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-04-08 00:01:36 -0500

    feat: Create this file to set up various note sequence tests

---
+++ super_mario_piezo/music_test.h
@@ -0,0 +1,24 @@
+#include "note.h"
+
+// up to DS8 is defined
+Note noteRunThrough[] = {
+{NOTE_B0, 4},
+{NOTE_C1, 4},
+{NOTE_CS1, 4},
+{NOTE_D1, 4},
+{NOTE_DS1, 4},
+{NOTE_E1, 4},
+{NOTE_F1, 4},
+{NOTE_FS1, 4},
+{NOTE_G1, 4},
+{NOTE_GS1, 4},
+{NOTE_A1, 4},
+{NOTE_AS1, 4},
+{NOTE_B1, 4},
+{NOTE_C2, 4}
+};
+
+
+
+
+
