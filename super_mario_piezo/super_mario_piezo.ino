dc61061e74561ead9abab686f02b0ed75f6b0199
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-04-09 08:36:58 -0500

    test: Debugging new code

--- super_mario_piezo/super_mario_piezo.ino
+++ super_mario_piezo/super_mario_piezo.ino
@@ -1,30 +1,41 @@
 #include "note.h"
+#include "music_test.h"
 #include "mario.h"
 
 #define melodyPin 3
 #define SECONDS_PER_MINUTE 60
 #define BPM 150
 
+Note test[] = {{NOTE_C4, 4}, {NOTE_B4, 4}};
+
 void setup(void)
 {
+  Serial.begin(9600);
   pinMode(melodyPin, OUTPUT);   //buzzer
 }
 void loop()
 {
-  play(mario_overworld_melody);
-  play(mario_underworld_melody);
+   play(test);
+  // play(mario_overworld_melody);
+  // play(mario_underworld_melody);
 }
 
 void play(Note song[]) {
   // iterate over the notes of the melody:
-    int size = sizeof(song) / sizeof(song[0]);
+    int sizetemp = sizeof(song);
+    int sizeeach = sizeof(song[0]);
+        Serial.println(sizetemp);
+
+    int size = sizeof(song);
+    Serial.println(size);
     for (int thisNote = 0; thisNote < size; thisNote++) {
       // note duration in milliseconds
       int beatsPerMeasure = 4;
       // currently assumes quarter note gets the beat
       int beatsPerSecond = (int)(BPM / SECONDS_PER_MINUTE);
       int secondsPerMeasure = (int) beatsPerMeasure/beatsPerSecond;
-      int noteDuration = (int) 1E6 * secondsPerMeasure / (song[thisNote].duration + 0.);
+      int noteDuration = (int) 1000000 * secondsPerMeasure / (song[thisNote].duration + 0.);
+      Serial.println(noteDuration);
       buzz(melodyPin, song[thisNote].pitch, noteDuration);
 
       // to separate the notes, set a minimum time between them. + 30% seems to work well.
@@ -36,7 +47,7 @@ void play(Note song[]) {
 
 void buzz(int targetPin, long frequency, long length) {
   // 1E6 microseconds, divided by freq, div by 2 for 50% duty cycle
-  long pulseWidth = 1E6 / frequency / 2;
+  long pulseWidth = 1000000 / frequency / 2;
   // total cycles = frequency (cycle/sec) * number of seconds 
   long numCycles = frequency * length;
   for (long i = 0; i < numCycles; i++) { 
