87ad36d50b6a94e6368f50784bcb3dadba77cfbf
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-04-10 13:29:14 -0500

    feat: Add starter code for basic Webclient and start modifying (mac, ip, etc)

---
+++ WebClient/WebClient.ino
@@ -0,0 +1,71 @@
+/*
+  Web client
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13 (Digital I/O
+
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x10, 0x02, 0xD1 };
+char server[] = "www.google.com";    // uses DNS
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192, 168, 1, 255);
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  Serial.println("connecting...");
+
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+}
+
+void loop() {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+}
+
