d6ad8e0209e454017cd886262c88f430eb8fd8fb
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-04-13 20:21:03 -0500

    feat: Add write to SD card of downloaded index.html file
    Print out the file to Serial on setup(), if exists

--- EthernetShield2/WebClient/WebClient.ino
+++ EthernetShield2/WebClient/WebClient.ino
@@ -15,11 +15,14 @@
 #include <SPI.h>
 #include <EEPROM.h>
 #include <stdio.h>
+#include <SD.h>
 
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x10, 0x02, 0xD1 };
 char server[] = "www.google.com";    // uses DNS
 int EEPROM_COUNTER = 0;
+File myFile;
+bool SDcardFound;
 
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192, 168, 1, 255);
@@ -34,6 +37,8 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+  // disable w5500 SPI
+  // digitalWrite(10,HIGH);
 
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
@@ -45,6 +50,19 @@ void setup() {
   // give the Ethernet shield a second to initialize:
   delay(1000);
   Serial.println("connecting...");
+
+  // disable w5500 SPI wile setting up SD
+  // digitalWrite(10,HIGH);
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    SDcardFound = false;
+  } else {
+    Serial.println("initialization done.");
+    SDcardFound = true;
+  }
 
   // if you get a connection, report back via serial:
   if (client.connect(server, 80)) {
@@ -60,13 +78,27 @@ void setup() {
     Serial.println("connection failed");
   }
   // print out what is stored in EEPROM:
-  Serial.println("setup() running. Data stored in EEPROM:");
+  Serial.println("\nData stored in EEPROM:\n");
   int addr = 0;
   for (addr=0; addr<1024; addr++) {
     char c = EEPROM.read(addr);
     Serial.print(c); 
   }
-  
+  Serial.println("\nEnd of Data stored in EEPROM:\n");
+
+  if (SD.exists("index.txt")) {
+    Serial.println("\n\nindex.txt exists. file contents: \n");
+   // read from the file until there's nothing else in it:
+     myFile = SD.open("index.txt");
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    myFile.close();
+    // SD.remove("index.txt");
+    Serial.println("Removing index.txt...");
+  } else {
+    Serial.println("index.txt doesn't exist.");
+  }
 }
 
 void loop() {
@@ -74,17 +106,18 @@ void loop() {
   // from the server, read them and print them:
   if (client.available()) {
     char c = client.read();
-    // 1024 bytes EEPROM on the
+    // 1024 bytes EEPROM - write first 1024 chars
     if (EEPROM_COUNTER < 1024) {
-//      char buffer[20];
-//      sprintf(buffer, "EEPROM COUNTER %i", EEPROM_COUNTER);
-//      Serial.println(buffer);
       EEPROM.write(EEPROM_COUNTER, c);
+      EEPROM_COUNTER++;
     }
-    EEPROM_COUNTER++;
-    // Serial.print(c);
+    // write the result to SD card
+    if (SDcardFound) {
+      myFile = SD.open("index.txt", FILE_WRITE);
+      myFile.write(c);
+      myFile.close();
+    }
   }
-
   // if the server's disconnected, stop the client:
   if (!client.connected()) {
     Serial.println();
